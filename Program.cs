e32d57f8cf14fce1a22375d17fff09b9f24863b3
prasadacharya <prasada@mail.usf.edu>
2020-03-03 23:58:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace defangedIPAddress
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string address = "255.100.50.0";
+            defangedIPaddress(address);
+        }
+        static void defangedIPaddress(string S)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] is '.')
+                {
+                    sb.Append("[.]");
+                }
+                else
+                {
+                    sb.Append(S[i]);
+                }
+            }
+
+            Console.WriteLine(sb);
+
+
+        }
+
+    }
+}
+
+
+
+
